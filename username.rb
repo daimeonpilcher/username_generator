f13937eb43ad13af2d8a1cbe8e20e6b3f918591e
DAIMEON PILCHER <daimeon.pilcher@gmail.com>
2015-06-02 17:19:02 -0700

    question 1 solved

---
+++ username.rb
@@ -0,0 +1,6 @@
+#1 Create a unary function generate_username1 that accepts a user's first name.
+	# return only the first character
+	# make it lowercase
+def generate_username1(firstName)
+	name = firstName[0].downcase
+end
